4a5c8ec86ef6f149dd2a04d0f7982c75195a8640
Abir210-bit <64340995+Abir210-bit@users.noreply.github.com>
2023-11-21 17:30:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,48 +5,49 @@ import Footer from '@components/Footer';
 
 export default function Home() {
   return  (
-    <div>
+    <div style='max-width: 600px;margin: 0 auto; padding: 20px;'>
       <header>
-        <h1>Your Name</h1>
-        <p>Job Title</p>
+        <h1>Marghni Abir</h1>
+        <p>Développeur Web</p>
       </header>
       
       <section>
-        <h2>Contact Information</h2>
-        <p>Email: your.email@example.com</p>
-        <p>Phone: (123) 456-7890</p>
-        <p>LinkedIn: linkedin.com/in/yourprofile</p>
-        {/* Add more contact information as needed */}
+        <h2>Informations</h2>
+        <p>Email: marghniabir5@gmail.com</p>
+        <p>Phone: 20881568</p>
+        <p>LinkedIn: linkedin.com/in/abirmarghni</p>
       </section>
 
       <section>
-        <h2>Summary</h2>
+        <h2>Etudes</h2>
         <p>
-          A brief summary of your professional background, skills, and goals.
+          <strong>université Virtuelle de Tunis (UVT)</strong> 13 Rue Ibn Nadim ,Montplaisir, Tunis Cité Monplaisir, 1073
         </p>
-      </section>
-
-      <section>
-        <h2>Education</h2>
-        <p>
-          <strong>University Name</strong> - Degree, Graduation Year
-        </p>
+      <p>Ingenierie de logiciel open source</p>
         {/* Add more education details as needed */}
       </section>
 
       <section>
         <h2>Experience</h2>
         <p>
-          <strong>Job Title</strong> - Company Name, Duration
+          <strong>Developpeur PHP</strong> - NEXT STEP IT, Tunis
         </p>
         <p>
-          Description of responsibilities and achievements in this role.
+         - Identifier, comprendre et analyser les besoins spécifiques d’un client potentiel <br/>
+         - Concevoir et la programmation d’un site internet, d’un module ou d’un développement spécifique  <br/>
+         - La capacité à élaborer plusieurs tests de vérification  <br/>
+         - Corriger des bugs.  
         </p>
         {/* Add more experience details as needed */}
       </section>
 
-      {/* Add more sections for skills, projects, certifications, etc. as needed */}
-
+        <section>
+        <h2>Compétences</h2>
+        <p> <strong>PHP</strong> </p>
+         <p> <strong>Odoo</strong> </p>
+      
+        {/* Add more experience details as needed */}
+      </section>
       <footer>
         <p>References available upon request</p>
       </footer>
