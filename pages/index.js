8a4ba2b37d246e60f89841ec2057b0ceaa4bf7bb
Abir210-bit <64340995+Abir210-bit@users.noreply.github.com>
2023-11-21 16:17:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,34 +6,42 @@ import Footer from '@components/Footer';
 export default function Home() {
   return (
     <div className="container">
-      <main>
-        <Header title="Marghni Abir - Curriculum Vitae" />
+      <Head>
+        <title>Marghni Abir - Curriculum Vitae</title>
+      </Head>
 
-        <section>
+      <Header title="Marghni Abir - Curriculum Vitae" />
+
+      <main>
+        <article>
           <h2>Personal Information</h2>
           <p>Name: Abir</p>
           <p>Email: marghniabir5@gmail.com</p>
           <p>Tel: 20881568</p>
+        </article>
 
-         <h2>Education</h2>
+        <article>
+          <h2>Education</h2>
           <p>Degree in Computer Science - University Name, Year</p>
+        </article>
 
-         <h2>Work Experience</h2>
+        <article>
+          <h2>Work Experience</h2>
           <p>Software Developer - Next Step, 2021</p>
           <p>Software Developer - SIRAT, 2020</p>
+        </article>
 
-         <h2>Skills</h2>
+        <article>
+          <h2>Skills</h2>
           <ul>
             <li>JavaScript</li>
             <li>React.js</li>
             <li>PHP</li>
           </ul>
-       <h2>Languages</h2>
+        </article>
+
+        <article>
+          <h2>Languages</h2>
           <p>English - Fluent</p>
           <p>French - Intermediate</p>
-      </main>
-
-      <Footer />
-    </div>
-  );
-}
+        </article
