0e43792dda1dcfff2b16a087fdd3b59ff94439b2
Abir210-bit <64340995+Abir210-bit@users.noreply.github.com>
2023-11-21 11:49:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,45 +7,48 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Your CV - Next.js</title>
+        <title>MY CV </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Your Name - Curriculum Vitae" />
+      <main style={mainStyle}>
+        <Header title="Marghni Abir - Curriculum Vitae" style={headerStyle} />
 
-        <section>
-          <h2>Personal Information</h2>
-          <p>Name: Your Name</p>
-          <p>Email: your.email@example.com</p>
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Personal Information</h2>
+          <p style={infoStyle}>Name: Abir</p>
+          <p style={infoStyle}>Email: marghniabir5@gmail.com</p>
+          <p style={infoStyle}>Tel: 20881568</p>
           {/* Add more personal information as needed */}
         </section>
 
-        <section>
-          <h2>Education</h2>
-          <p>Degree in Computer Science - University Name, Year</p>
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Education</h2>
+          <p style={infoStyle}>Degree in Computer Science - University Name, Year</p>
           {/* Add more education details as needed */}
         </section>
 
-        <section>
-          <h2>Work Experience</h2>
-          <p>Software Developer - Company Name, Year</p>
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Work Experience</h2>
+          <p style={infoStyle}>Software Developer - Next Step, 2021</p>
+          <p style={infoStyle}>Software Developer - SIRAT, 2020</p>
           {/* Add more work experience details as needed */}
         </section>
 
-        <section>
-          <h2>Skills</h2>
-          <ul>
-            <li>JavaScript</li>
-            <li>React.js</li>
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Skills</h2>
+          <ul style={listStyle}>
+            <li style={infoStyle}>JavaScript</li>
+            <li style={infoStyle}>React.js</li>
+            <li style={infoStyle}>PHP</li>
             {/* Add more skills as needed */}
           </ul>
         </section>
 
-        <section>
-          <h2>Languages</h2>
-          <p>English - Fluent</p>
-          <p>French - Intermediate</p>
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Languages</h2>
+          <p style={infoStyle}>English - Fluent</p>
+          <p style={infoStyle}>French - Intermediate</p>
           {/* Add more language details as needed */}
         </section>
       </main>
@@ -54,3 +57,36 @@ export default function Home() {
     </div>
   );
 }
+
+// Styles
+const mainStyle = {
+  padding: '20px',
+};
+
+const headerStyle = {
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+  textAlign: 'center',
+};
+
+const sectionStyle = {
+  margin: '20px 0',
+  padding: '10px',
+  border: '1px solid #ddd',
+  borderRadius: '5px',
+};
+
+const header2Style = {
+  color: '#333',
+};
+
+const infoStyle = {
+  color: '#555',
+};
+
+const listStyle = {
+  listStyleType: 'none',
+  padding: 0,
+  margin: 0,
+};
