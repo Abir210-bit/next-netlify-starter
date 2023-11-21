0a1ac157b393700a1c58a8982529196a81274cd8
Abir210-bit <64340995+Abir210-bit@users.noreply.github.com>
2023-11-21 11:59:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,51 +3,6 @@ import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <main style={styles.main}>
-        <Header title="Marghni Abir - Curriculum Vitae" style={styles.header} />
-
-        <section style={styles.section}>
-          <h2 style={styles.header2}>Personal Information</h2>
-          <p style={styles.info}>Name: Abir</p>
-          <p style={styles.info}>Email: marghniabir5@gmail.com</p>
-          <p style={styles.info}>Tel: 20881568</p>
-        </section>
-
-        <section style={styles.section}>
-          <h2 style={styles.header2}>Education</h2>
-          <p style={styles.info}>Degree in Computer Science - University Name, Year</p>
-        </section>
-
-        <section style={styles.section}>
-          <h2 style={styles.header2}>Work Experience</h2>
-          <p style={styles.info}>Software Developer - Next Step, 2021</p>
-          <p style={styles.info}>Software Developer - SIRAT, 2020</p>
-        </section>
-
-        <section style={styles.section}>
-          <h2 style={styles.header2}>Skills</h2>
-          <ul style={styles.list}>
-            <li style={styles.info}>JavaScript</li>
-            <li style={styles.info}>React.js</li>
-            <li style={styles.info}>PHP</li>
-          </ul>
-        </section>
-
-        <section style={styles.section}>
-          <h2 style={styles.header2}>Languages</h2>
-          <p style={styles.info}>English - Fluent</p>
-          <p style={styles.info}>French - Intermediate</p>
-        </section>
-      </main>
-
-      <Footer />
-    </div>
-  );
-}
-
 // Styles
 const mainStyle = {
   padding: '20px',
@@ -80,3 +35,48 @@ const listStyle = {
   padding: 0,
   margin: 0,
 };
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main style={mainStyle}>
+        <Header title="Marghni Abir - Curriculum Vitae" style={headerStyle} />
+
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Personal Information</h2>
+          <p style={infoStyle}>Name: Abir</p>
+          <p style={infoStyle}>Email: marghniabir5@gmail.com</p>
+          <p style={infoStyle}>Tel: 20881568</p>
+        </section>
+
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Education</h2>
+          <p style={infoStyle}>Degree in Computer Science - University Name, Year</p>
+        </section>
+
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Work Experience</h2>
+          <p style={infoStyle}>Software Developer - Next Step, 2021</p>
+          <p style={infoStyle}>Software Developer - SIRAT, 2020</p>
+        </section>
+
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Skills</h2>
+          <ul style={listStyle}>
+            <li style={infoStyle}>JavaScript</li>
+            <li style={infoStyle}>React.js</li>
+            <li style={infoStyle}>PHP</li>
+          </ul>
+        </section>
+
+        <section style={sectionStyle}>
+          <h2 style={header2Style}>Languages</h2>
+          <p style={infoStyle}>English - Fluent</p>
+          <p style={infoStyle}>French - Intermediate</p>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
